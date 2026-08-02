273a955e4c0ee059a084b6eadba40cf46ad815a6
Pritam Dey <dey18944@gmail.com>
2026-08-02 11:12:37 +0530

    Time: 1611 ms (16.24%), Space: 19.3 MB (79.82%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,9 +1,12 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-           def player(left,right):
+        def player(left,right):
             if left==right:
-                return nums[left]
-            left_choice=nums[left]-player(left+1,right)
-            right_choice=nums[right]-player(left,right-1)
-            return max (left_choice,right_choice)
-           return player (0,len(nums)-1)>=0
+                return nums [left]
+            left_val = nums[left]-player(left+1,right)
+            right_val = nums[right]-player(left,right-1)
+            return max (left_val,right_val)
+        return player (0, len(nums)-1)>=0
+              
+                 
+          
